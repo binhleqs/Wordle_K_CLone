e38a43948d5fe73c2c5d5bcd122063566fa85729
binhleqs <binhuuuo96@gmail.com>
2024-02-23 11:04:01 +0700

    "Newsetupfile"

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'Wordle_K_CLone',
+    packages = ['Wordle_K_CLone'],
+    version = 'Test',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/binhleqs/Wordle_K_CLone.git',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
